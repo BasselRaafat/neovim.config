8aef953c92dc1a39dce0bc5019f363ec8cb39ac2
Bassel Raafat <basselr11@gmail.com>
2025-02-14 22:59:06 +0200

    changes to indent-blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -2,10 +2,9 @@ return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
 	---@module "ibl"
-	---@diagnostic disable-next-line: undefined-doc-name
 	---@type ibl.config
-	---@diagnostic disable-next-line: assign-type-mismatch
-	config = function()
-		require("ibl").setup({})
+	opts = {},
+	config = function(_, opts)
+		require("ibl").setup(opts)
 	end,
 }
