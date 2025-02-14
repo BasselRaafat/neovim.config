2df34cb1b1535c8d4d87d4632d097f08a708291d
Bassel Raafat <basselr11@gmail.com>
2025-02-15 01:04:18 +0200

    changes to indent-blankline highlight group color

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,8 +3,17 @@ return {
 	main = "ibl",
 	---@module "ibl"
 	---@type ibl.config
-	opts = {},
+	opts = {
+		indent = {
+			char = "▏", -- This is a slightly thinner char than the default one, check :help ibl.config.indent.char
+		},
+		scope = {
+			show_start = false,
+			show_end = false,
+		},
+	},
 	config = function(_, opts)
 		require("ibl").setup(opts)
+		vim.api.nvim_set_hl(0, "IblIdent", { fg = "#555555", nocombine = true })
 	end,
 }
